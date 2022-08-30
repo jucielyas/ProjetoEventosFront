9ff92de9f13a59b25476db1d79b0f2cf65eab4e3
juciely <jussi.jpg@outlook.com.br>
2022-08-30 19:43:37 -0300

    primeiro commit

--- eventos-front/README.md
+++ eventos-front/README.md
@@ -1,4 +1,4 @@
-# ProjetoEventosFront
+# EventosFront
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 14.0.0-next.4.
 
--- eventos-front/src/app/app.component.spec.ts
+++ eventos-front/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'projeto-eventos-front'`, () => {
+  it(`should have as title 'eventos-front'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('projeto-eventos-front');
+    expect(app.title).toEqual('eventos-front');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('projeto-eventos-front app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('eventos-front app is running!');
   });
 });
